7d46f4068d9847c5d8351d126f93e487c3ca81c6
Francis Chung <francis.c.chung@gmail.com>
2024-12-10 15:16:59 +1100

    Objects Arrays and Tuples

---
+++ examples/03 Objects Arrays and Tuples/objects & property types.ts
@@ -0,0 +1,34 @@
+
+
+const myCar = {
+    make : "Toytota",
+    model : "Corolla",
+    year : 2024
+}
+
+let car : {
+    make: string
+    model: string
+    year: number
+} = myCar
+
+function printCar( car : {
+    make : string
+    model : string
+    year : number
+    chargeVoltage ?: number
+}) {
+    let str = "${car.make}, ${car.model}, ${car.year}"
+    if (typeof(car.chargeVoltage) === "number")
+        str += ` ${car.chargeVoltage} `
+}
+
+printCar(car)
+
+printCar({
+        make : "Toytota",
+        model : "Corolla",
+        year : 2024,
+        chargeVoltage : 220
+    }
+)
