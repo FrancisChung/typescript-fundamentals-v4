3eaa3899c44814a9495abe50be7f0f88e04c0274
Francis Chung <francis.c.chung@gmail.com>
2024-12-10 16:34:18 +1100

    Refactored

--- examples/04 index signatures/index signatures.ts
+++ examples/04 index signatures/index signatures.ts
@@ -1,16 +1,31 @@
-const phoneNumbers  = {
-    home: {country: "+1",  area: "211", number: "1234-543-5232"},
-    work: {country: "+1",  area: "211", number: "1234-543-5232"},
-    fax: {country: "+1",  area: "211", number: "1234-543-5232"}
-}
-
-
-
-const x : {[k:string] : string} = {}
 const phones : {
-    [k: string ]: {
+    mobile:  {
         country: string
         area: string
         number: string
     }
-} = {}
+    [k: string ]: {
+        country: string
+        area: string
+        number: string
+    }}  = {
+    home: {country: "+1",  area: "211", number: "1234-543-5232"},
+    work: {country: "+1",  area: "211", number: "1234-543-5232"},
+    fax: {country: "+1",  area: "211", number: "1234-543-5232"},
+    mobile: {country: "+1",  area: "211", number: "1234-543-5232"}
+}
+
+console.log(phones.home.country)
+console.log(phones.custom_1)
+
+const x : {[k:string] : string} = {}
+x.foo = "bar"
+
+
+// const phones : {
+//     [k: string ]: {
+//         country: string
+//         area: string
+//         number: string
+//     }
+// } = {}
